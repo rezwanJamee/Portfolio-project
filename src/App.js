59c6e948cf71b539886eea6cc48d57f9c6adc8d2
RezwanJamee <rezwan.jamee@gmail.com>
2021-03-13 21:27:23 +1100

    Services complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
+import Services from './components/Services';
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
     <Navbar />
     <Header />
     <AboutMe />
+    <Services />
     </>
   );
 }
